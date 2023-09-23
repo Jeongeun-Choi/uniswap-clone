ac9b67a612ee2c2941a62ac5477ffad9b0334d5e
Jeongeun-Choi <cje3080@gmail.com>
2023-09-23 14:40:59 +0900

    fix: Input 컴포넌트 커스텀 속성 추가 및 수정

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,19 +1,46 @@
-import { ReactElement } from "react";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { HTMLAttributes, ReactElement } from "react";
 
-interface InputProps {
-  customClassName?: string;
-  leftIcon?: ReactElement;
+interface InputProps extends HTMLAttributes<HTMLInputElement> {
+  width?: string;
+  height?: string;
+  customInputClassName?: string;
+  borderColor?: string;
+  bgColor?: string;
+  hasLeftIcon?: boolean;
+  customLeftIcon?: ReactElement;
   rightIcon?: ReactElement;
 }
 
-function Input({ customClassName, leftIcon, rightIcon }: InputProps) {
+function Input({
+  customInputClassName,
+  width,
+  height,
+  borderColor,
+  bgColor,
+  hasLeftIcon,
+  customLeftIcon,
+  rightIcon,
+  placeholder,
+  ...rest
+}: InputProps) {
   return (
-    <div className="w-[480px] px-4 py-3 basic_border rounded-[12px] border-gray-200 bg-gray-100">
+    <div
+      className={`px-4 py-3 basic_border rounded-[12px] ${width} ${height} ${borderColor} ${bgColor}`}
+    >
       <div className="flex items-center">
-        {leftIcon}
+        {hasLeftIcon &&
+          (customLeftIcon || (
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="text-gray-700 mr-1.5"
+            />
+          ))}
         <input
-          placeholder="Search tokens and NFT collections"
-          className="text-gray-700 text-base font-medium bg-gray-100 outline-0"
+          placeholder={placeholder}
+          className={`outline-0 ${customInputClassName} ${bgColor}`}
+          {...rest}
         />
         {rightIcon}
       </div>
