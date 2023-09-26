5b4ebe6e2c9e5874121856ccc85de4fc9e9bd93c
Jeongeun-Choi <cje3080@gmail.com>
2023-09-27 02:26:37 +0900

    style: 스타일 관련 수정

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,13 +1,13 @@
 import { SelectTokenProps } from "../../common/types";
 
 function TokenBadge({ token, isSelected, onClickToken }: SelectTokenProps) {
+  const style = isSelected ? "bg-gray-200 pointer-events-none" : "";
+
   return (
     <li
       key={token.id}
       id={token.id.toString()}
-      className={`${
-        isSelected && "bg-gray-200 pointer-events-none"
-      } flex w-fit m-1 py-[5px] pl-1.5 pr-3 rounded-[18px] basic_border border-gray-300 hover:bg-gray-100 cursor-pointer`}
+      className={`${style} flex w-fit m-1 py-[5px] pl-1.5 pr-3 rounded-[18px] basic_border border-gray-300 hover:bg-gray-100 cursor-pointer`}
       onClick={onClickToken}
     >
       <div className="mr-2">{token.Icon?.({ width: 24, height: 24 })}</div>
--- src/components/TokenItem/index.tsx
+++ src/components/TokenItem/index.tsx
@@ -3,13 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { SelectTokenProps } from "../../common/types";
 
 function TokenItem({ token, isSelected, onClickToken }: SelectTokenProps) {
+  const style = isSelected ? "opacity-60 pointer-events-none" : "";
+
   return (
     <li
       key={token.id}
       id={token.id.toString()}
-      className={`${
-        isSelected && "opacity-60 pointer-events-none"
-      } grid grid-cols-list gap-4 px-5 py-2 hover:bg-gray-100 cursor-pointer`}
+      className={`${style} grid grid-cols-list gap-4 px-5 py-2 hover:bg-gray-100 cursor-pointer`}
       onClick={onClickToken}
     >
       <div>{token.Icon?.({ width: 36, height: 36 })}</div>
