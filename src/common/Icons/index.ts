33963682a48ba2d217947702d5435c13d974a379
Jeongeun-Choi <cje3080@gmail.com>
2023-09-26 19:59:44 +0900

    add: Icon SVG 컴포넌트 추가

---
+++ src/common/Icons/index.ts
@@ -0,0 +1,4 @@
+export { default as IconEthLogo } from "./IconEthLogo";
+export { default as IconUniswapLogo } from "./IconUniswapLogo";
+export { default as IconUSDCLogo } from "./IconUSDCLogo";
+export { default as IconWBTCLogo } from "./IconWBTCLogo";
